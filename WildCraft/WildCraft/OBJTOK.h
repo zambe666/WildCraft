17c503f490879a997b12a6f3d05321929fa69d55
zambe666 <zambe666@gmail.com>
2016-12-10 16:50:47 +0900

    제외된 파일들 추가

---
+++ WildCraft/WildCraft/OBJTOK.h
@@ -0,0 +1,21 @@
+#pragma once
+// .obj 파일
+#define ID_MTLLIB				("mtllib")
+#define ID_GROUP				("g")
+#define ID_VERTEX				("v")
+#define ID_TVERTEX				("vt")
+#define ID_NVERTEX				("vn")
+#define ID_USEMTL				("usemtl")
+#define ID_UNKNOWN1				("s")
+#define ID_FACE					("f")
+
+
+// .mtl 파일
+#define ID_NEWMTL				("newmtl")
+#define ID_AMBIENT				("Ka")
+#define ID_DIFFUSE				("Kd")
+#define ID_SPECULAR				("Ks")
+#define ID_ALPHA				("d")
+#define ID_POWER				("Ns")
+#define ID_UNKNOWN4				("illum")
+#define ID_TEXTURE				("map_Kd")
